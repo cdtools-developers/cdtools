a97e4cdf39590049643de1845aadd0437ad09b22
Abe Levitan <abraham.levitan@psi.ch>
2026-01-14 17:01:11 +0100

    Add licensed near-field ptycho data to enable testing

---
+++ examples/near_field_ptycho.py
@@ -0,0 +1,56 @@
+import cdtools
+from matplotlib import pyplot as plt
+
+filename = 'example_data/PETRAIII_P25_Near_Field_Ptycho.cxi'
+dataset = cdtools.datasets.Ptycho2DDataset.from_cxi(filename)
+
+dataset.inspect()
+plt.show()
+
+# Setting near_field equal to True uses an angular spectrum propagator in
+# lieu of the default Fourier-transform propagator for far-field ptychography.
+#
+# If propagation_distance is not set, it assumes that the geometry is
+# a standard near-field geometry with flat illumination wavefronts, and
+# pulls the sample to detector distance from dataset.distance
+#
+# If propagation_distance is set, it assumes a Fresnel scaling theorem
+# geometry with:
+#
+# - distance (from the dataset): The sample-to-detector distance
+# - propagation_distance: The focus-to-sample distance
+# 
+model = cdtools.models.FancyPtycho.from_dataset(
+    dataset,
+    n_modes=1,
+    near_field=True,
+    propagation_distance=3.65e-3, # 3.65 downstream from focus
+    units='um', # Set the units for the live plots
+    obj_view_crop=-35,
+)
+
+device = 'cuda'
+model.to(device=device)
+dataset.get_as(device=device)
+
+model.inspect(dataset)
+
+recon = cdtools.reconstructors.AdamReconstructor(model, dataset)
+
+for loss in recon.optimize(100, lr=0.04, batch_size=10):
+    print(model.report())
+    # Plotting is expensive, so we only do it every tenth epoch
+    if model.epoch % 10 == 0:
+        model.inspect(dataset)
+
+for loss in recon.optimize(50, lr=0.005, batch_size=50):
+    print(model.report())
+    if model.epoch % 10 == 0:
+        model.inspect(dataset)
+
+# This orthogonalizes the recovered probe modes
+model.tidy_probes()
+
+model.inspect(dataset)
+model.compare(dataset)
+plt.show()
